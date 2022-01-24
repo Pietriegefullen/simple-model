495d7da537cddaa7a85269a460e8168eecb8ef8a
lara <lara.kaiser@uni-hamburg.de>
2022-01-24 14:57:05 +0100

    added thermodynamics

--- faster/CONSTANTS.py
+++ faster/CONSTANTS.py
@@ -9,3 +9,5 @@ STANDARD_TEMPERATURE = 25.0 + KELVIN
 SPECIMEN_TEMPERATURE = 4.0  + KELVIN
 
 SOIL_DENSITY = 1.3 # g/cm3
+
+GAS_CONSTANT = 8.31446261815324 	   # in J⋅K−1⋅mol−1, Gaskonstante ,Einheit passt zur DGmin
