aabfdd86e343af296b10222403b9b43567c96510
lara <lara.kaiser@uni-hamburg.de>
2022-04-25 10:58:58 +0200

    comparison_parameters normalized

--- faster/comparison_Parameters.py
+++ faster/comparison_Parameters.py
@@ -6,6 +6,12 @@ import argparse
 from datetime import datetime
 import json
 
+import yellowbrick
+from yellowbrick.features import ParallelCoordinates
+from yellowbrick.datasets import load_occupancy
+
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -77,42 +83,133 @@ for file in os.listdir(directory):
 
          
 
-         get_out_indices =[]
-         for specimen_index, optimal_parameter_dict in all_speciemen_dict.items():
-             row = [specimen_index] + list(optimal_parameter_dict.values())
-             all_parameter_list.append(row)
+get_out_indices =[]
+for specimen_index, optimal_parameter_dict in all_speciemen_dict.items():
+    row = [specimen_index] + list(optimal_parameter_dict.values())
+    all_parameter_list.append(row)
 
 columns_list = ['specimen_index'] + columns
 get_out = ['temperature', 'C', 'DOC', 'pH', 'weight', 'H2O','water']
 get_out_indices = list(reversed(sorted([columns_list.index(name) for name in get_out])))
     
 for column_index in get_out_indices:
-    del all_parameter_list[column_index]
     del columns_list[column_index]
+    for sp in range(len(all_parameter_list)):
+        del all_parameter_list[sp][column_index]
+    
 
-# for name in get_out:
-#     columns_list.remove(name)
 
 
-# for c, name in enumerate(columns_list):
-#     print(name, all_parameter_list[0][c])
     
   
-            
-         
+#print(len(all_parameter_list[0])) 
+#print(len(columns_list)) 
+
+
             
 specimen_df = pd.DataFrame(all_parameter_list, 
-                           columns = ['specimen_index'] + columns)
+                            columns = columns_list)
+
+minmaxdict = {}
+for col in columns_list[1:]:
+    col_min = min(specimen_df[col])
+    col_max = max(specimen_df[col])
+    minmaxdict.update({col: [col_min, col_max]})
+    specimen_df[col] = (specimen_df[col] - col_min) /(col_max - col_min)
+# Calculate the limits on the data
 
 
-ax = parallel_coordinates(specimen_df,"specimen_index", cols= columns)
+
+
+columns_list.remove('specimen_index')
+ax = parallel_coordinates(specimen_df,"specimen_index", cols= columns_list)
 ax.get_legend().remove()
 plt.xticks(rotation=90)
 plt.show()
          
+ 
+# plots für die verschiedenen Mikrobengruppen        
+Fe3_columns_list = [s for s in columns_list if "Fe3" in s]
+ax = parallel_coordinates(specimen_df,"specimen_index", cols= Fe3_columns_list)
+ax.get_legend().remove()
+plt.xticks(rotation=90)
+plt.show() 
+        
+
+Ferm_columns_list = [s for s in columns_list if "Ferm" in s]
+ax = parallel_coordinates(specimen_df,"specimen_index", cols= Ferm_columns_list)
+ax.get_legend().remove()
+plt.xticks(rotation=90)
+plt.show() 
+        
+
+Homo_columns_list = [s for s in columns_list if "Homo" in s]
+ax = parallel_coordinates(specimen_df,"specimen_index", cols= Homo_columns_list)
+ax.get_legend().remove()
+plt.xticks(rotation=90)
+plt.show() 
+        
+
+
+Hydro_columns_list = [s for s in columns_list if "Hydro" in s]
+ax = parallel_coordinates(specimen_df,"specimen_index", cols= Hydro_columns_list)
+ax.get_legend().remove()
+plt.xticks(rotation=90)
+plt.show() 
+        
+      
+Ac_columns_list = [s for s in columns_list if "Ac" in s]
+Ac_columns_list.remove('Acetate')
+ax = parallel_coordinates(specimen_df,"specimen_index", cols= Ac_columns_list)
+ax.get_legend().remove()
+plt.xticks(rotation=90)
+plt.show() 
+        
+      
+        
+      
+        
+      
+        
+      
+        
+      
+        
+      
+        
+      
+        
+      
+        
+      
+# anderer ansatz der dumm ist
          
-         
-         
+
+# # Load the classification data set
+# X= specimen_df
+# y1 = np.ones(len(specimen_df['specimen_index']))
+# print(y1)
+
+# #X,y = load_occupancy()
+
+# print(X)
+# #print(y)
+# # Specify the features of interest and the classes of the target
+# features = columns_list
+# #classes = ["unoccupied", "occupied"]
+
+# # Instantiate the visualizer
+# visualizer = ParallelCoordinates(
+#       features=features,
+#     normalize='standard', sample=0.05, shuffle=True,
+# )
+
+# # Fit the visualizer and display it
+# visualizer.fit_transform(specimen_df[features], y1)
+# #ax = plt.gca()
+# plt.xticks(rotation=90)
+# visualizer.show() 
+
          
          
          
