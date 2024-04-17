d009fb545e7fd38aeb71852a7ccc1e23777cbdbf
Martin GPU <martin.legeland@tuhh.de>
2024-04-17 20:49:11 +0200

    include excel files

--- faster/compare_models.py
+++ faster/compare_models.py
@@ -103,4 +103,4 @@ def fit(include_samples = None, exclude_samples = None):
                     print()
     
 if __name__ == '__main__':
-    fit(['1352'])
+    fit(['1354'])
--- faster/model.py
+++ faster/model.py
@@ -216,7 +216,9 @@ if __name__ == '__main__':
     #1/0
 
     results_folder = 'fit_13526_2024-04-16--13-53-49'
+    results_folder = 'fit_13535_2024-04-17--09-09-04'
     results_folder = 'fit_13515_13516_2024-04-16--13-52-38'
+    results_folder = 'fit_13546_2024-04-17--20-38-32'
 
     parameter_source = os.path.join(USER_VARIABLES.LOG_DIRECTORY, results_folder)
     best_loss, p = get_best_loss_parameters(parameter_source)
