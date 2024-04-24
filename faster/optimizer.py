6c714276273ef496bc2736c9ea273d249527265d
Martin GPU <martin.legeland@tuhh.de>
2024-04-24 09:14:04 +0200

    more decimals in file name

--- faster/optimizer.py
+++ faster/optimizer.py
@@ -193,7 +193,7 @@ class Objective():
         return total_loss
   
     def file_name(self, total_loss):
-        return f'call_{self._call_count:03d}_loss_{total_loss:.2f}'
+        return f'call_{self._call_count:03d}_loss_{total_loss:7.4f}'
 
     def get_callback(self):
         def callback(intermediate_result):
