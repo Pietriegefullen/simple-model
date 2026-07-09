ecf85780ced8541577c3f61a964cf17898ac6df8
Lara Sophia Kaiser <u290380@marin03.cen.uni-hamburg.de>
2026-07-09 13:34:59 +0200

    results

--- faster/compare_models.py
+++ faster/compare_models.py
@@ -185,13 +185,12 @@ if __name__ == '__main__':
         narrower_range = False
     
     loaded_range = None
+    replica_name = '456'[split]
     if narrower_range:
-        replica_name = '456'[split]
         loaded_range = load_parameter_range(default_sample, 
                                             replica_name, 
                                             default_model_type,
                                             best_N = best_N)
-            
 
     #log = True
     #if 'log' in sys.argv:
