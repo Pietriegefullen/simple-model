22a97c12210c1a112961b327f1e9523d73e8341f
lara <lara.kaiser@uni-hamburg.de>
2026-08-21 13:51:34 +0200

    initial state for inocculated replicas

--- faster/main_file.py
+++ faster/main_file.py
@@ -174,7 +174,7 @@ if __name__ == '__main__':
     from plot import plot_Gibbs, plot_fit, plot_thermodynamics, plot_pathways
   
     model_type= 'complex'
-    sample_name =  '1373' #1353 1351, 1367, 1369, 1370, 
+    sample_name =  '2356' #1353 1351, 1367, 1369, 1370, 
     replica_name = 4 # 4?, 5?, 6?
     reset_Fe3 = None #2000 # set the day on which to reset Fe3 to initial value, None to omit reset
     log_co2 = False
@@ -207,8 +207,8 @@ if __name__ == '__main__':
             log = pathway_model.system_change_log
             
         #plot_Gibbs(log, 'Fe3')
-        #plot_fit(log)
-        plot_pathways(log)
+        plot_fit(log)
+        #plot_pathways(log)
         #plot_thermodynamics(log, 'Fe3')
 
         plt.show()
